a0378d98c424b1c6fdbaf14d3624c58ed18c6b9e
Joshua Erinosho <joshuaerinosho@gmail.com>
2022-05-27 16:11:47 +0100

    Fix compile issues

--- lib/src/pages/call.dart
+++ lib/src/pages/call.dart
@@ -50,7 +50,7 @@ class _CallPageState extends State<CallPage> {
     if (APP_ID.isEmpty) {
       setState(() {
         _infoStrings.add(
-          'APP_ID missing, please provide your APP_ID in settings.dart',
+          APP_ID,
         );
         _infoStrings.add('Agora Engine is not starting');
       });
@@ -61,7 +61,7 @@ class _CallPageState extends State<CallPage> {
     _addAgoraEventHandlers();
     await _engine.enableWebSdkInteroperability(true);
     VideoEncoderConfiguration configuration = VideoEncoderConfiguration();
-    configuration.dimensions = VideoDimensions(1920, 1080);
+    configuration.dimensions = VideoDimensions(width: 1920, height: 1080);
     await _engine.setVideoEncoderConfiguration(configuration);
     await _engine.joinChannel(Token, widget.channelName, null, 0);
   }
@@ -117,7 +117,10 @@ class _CallPageState extends State<CallPage> {
     if (widget.role == ClientRole.Broadcaster) {
       list.add(RtcLocalView.SurfaceView());
     }
-    _users.forEach((int uid) => list.add(RtcRemoteView.SurfaceView(uid: uid)));
+    _users.forEach((int uid) => list.add(RtcRemoteView.SurfaceView(
+          uid: uid,
+          channelId: 'test',
+        )));
     return list;
   }
 
